05547a6993cab16c71ffa9a19673c818585c0adb
Arnab Kumar Jha <arnab.jha@enpass.io>
2025-01-12 14:31:48 +0530

    Refactored Routes, Models and Controllers. Switched to V2

--- db/index.js
+++ db/index.js
@@ -1,21 +1,22 @@
 import mongoose from 'mongoose';
 
-const MONGO_URL = process.env.MONGO_URL; // Ensure this is correctly set in your environment variables
+const MONGO_URL = process.env.MONGO_URL;
+if (!MONGO_URL) {
+  throw new Error('MONGO_URL environment variable is not defined.');
+}
 
-// Utility function to connect to MongoDB
 const connectDB = async () => {
   try {
-    // Connect to MongoDB without the deprecated options
     await mongoose.connect(MONGO_URL, {
-      serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
-      socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
-      family: 4 // Use IPv4, skip trying IPv6
+      serverSelectionTimeoutMS: 5000,
+      socketTimeoutMS: 45000,
+      family: 4,
     });
     console.log('Successfully connected to MongoDB');
   } catch (error) {
-    console.error('Error connecting to MongoDB:', error);
-    console.error('Attempting to reconnect...');
-    setTimeout(connectDB, 5000); // Try to reconnect every 5 seconds if initial connection fails
+    console.error('Error connecting to MongoDB:', error.message);
+    console.error('Retrying in 5 seconds...');
+    setTimeout(connectDB, 5000);
   }
 };
 
