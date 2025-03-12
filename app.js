2056eab10c70640bc32a9d01dca8d8fd593f179e
Arnab Kumar Jha <arnab.jha@enpass.io>
2025-03-12 22:33:25 +0530

    CORS updated

--- app.js
+++ app.js
@@ -19,12 +19,10 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
-
-// app.use(cors({
-//   origin: 'https://placify-admin-frontend.vercel.app/',
-//   credentials: true,
-// }));
+app.use(cors({
+  origin: ['http://localhost:5173', 'https://placify-admin-frontend.vercel.app'],
+  credentials: true,
+}));
 
 app.use(helmet());
 app.use(limiter);
